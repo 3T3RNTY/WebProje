f7706b0134d07c3074d798bedce0f0117e12842f
Emirhan <bayrakemirhan9@gmail.com>
2026-02-24 10:02:46 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Web tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Emirhan Bayrak</p>
+      <p>Numara: 230541017</p>
+    </div>
+  )
+}
+
+export default App
